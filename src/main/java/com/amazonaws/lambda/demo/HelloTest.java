3a8bc1572bfb0fa3fbe4c77d73ff4db889c92539
raymondzhu2020 <58727770+raymondzhu2020@users.noreply.github.com>
2019-12-17 17:02:19 +0800

    Update HelloTest.java

--- src/main/java/com/amazonaws/lambda/demo/HelloTest.java
+++ src/main/java/com/amazonaws/lambda/demo/HelloTest.java
@@ -4,10 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 
-public class HelloTest implements RequestHandler<String, String> {
+public class HelloTest implements RequestHandler<Map<String,Object>, String> {
          
 	@Override
-	public String handleRequest(String input, Context context) {
+	public String handleRequest(Map<String,Object> input, Context context) {
 		// TODO Auto-generated method stub
 		System.out.println("Input: " +  input);
 		if (input != null || input.length() !=0)
